7dfc86666200433622d9b1f9efd55aa417aa727c
TheGuardianWolf <nano@pixelcollider.net>
2024-11-17 23:40:35 +1300

    Change scan interval

--- src/Defines.h
+++ src/Defines.h
@@ -8,6 +8,6 @@ const BLEUUID BLUEPER_CHARACTERISTIC_SET_DEVICE_NAME_ID("06e60583-1866-42cf-aeff
 const BLEUUID BLUEPER_CHARACTERISTIC_IS_CONFIGURABLE_ID("06e60583-1866-42cf-aeff-70cad32b727e");
 constexpr uint8_t LED_PIN = 2; // Change this to the pin connected to your LED
 constexpr uint32_t LOOP_TIME = 50;
-constexpr uint32_t SCAN_DURATION = 3;
+constexpr uint32_t SCAN_DURATION = 1;
 
 const std::string DEFAULT_BLUEPER_VALUE = "Blueper";
